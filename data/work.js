5230c1df83bfc4690ec64b0c4ff637e96f8b9527
RooseViper <roosemwandira@gmail.com>
2025-04-03 20:54:17 +0200

    Add: Added Mayham Mansion

--- data/work.js
+++ data/work.js
@@ -11,10 +11,10 @@ export const projects = [
             "ESCAPE ROOM"
         ],
         industry: "design industry",
-        solution: "Terror: Evocation II is a story driven survival Horror game set in Malawi, Africa. The game has 3 large maps and another semi-large map which is explored in the final chapter of the game. The first 3 maps of the game can be explored in any order depending on how Players want to explore the story but the story will not be affected and will remain the same. As far as gameplay, the game has many features such as a trackable map, Inventory management, In-game shop, Hiding, Crafting, In-App purchases, Loading checkpoints, Perks (Different unique abilities), Combat (both melee and long-range), 5 unique enemy AIs (Each with unique behavior and abilities) and multiple endings. The most unique aspect of this game is the Corruption system whereby the Player’s soul gets corrupted each time they die. Soul corruption changes the dynamic of the game and how enemies react to the player. For instance, there are some enemies that will mainly hunt down the player more when their soul is less corrupt. A higher soul corruption can also have side effects such as locked slots for the perks meaning players cannot use certain abilities when their soul is corrupt. Having high soul corruption also comes with other side effects such as vision impairment and slow movement. Most importantly, the level of corruption of the soul also dictates the endings of the game as currently the game has 3 endings. The game currently has a few thousand downloads and was covered by several YouTube channels.",
+        solution: "Mayham Mansion is a VR escape room game where players must escape a mysterious mansion after being captured by an enigmatic figure, all in pursuit of a grand prize. As the project lead in a 2-person team, I was responsible for level design, gameplay design, and storytelling. The game was developed exclusively for the Oculus Quest 2 & 3 and it focuses on immersive puzzles and an atmospheric experience.",
         link: "https://play.google.com/store/apps/details?id=com.TerrifikGameStudios.terrorevocationii",
         link_2: "",
-        anchorText: "Download Game(Google Play)",
+        anchorText: "",
         anchorText_2: "",
         date: "10 November 2022",
         images: [
@@ -25,11 +25,11 @@ export const projects = [
             "https://imgdb.net/storage/uploads/54d82555387bede4c2eaf0ac5c3acfed3cf2d51f8b2e19e158404f2622ff165b.png",
             "https://imgdb.net/storage/uploads/9b9d86a38a69af57878342441d0875effb075c7700ed0e793624278d872819e3.png"
         ],
-        videoLink:"https://www.youtube.com/embed/12QIjEZmsek",
+        videoLink:"https://www.youtube.com/embed/IEISZAxPy9U",
         videoTitle:"Terror: Evocation II Official Trailer",
-        itchEmbed: "https://itch.io/embed/3316118?border_width=2&bg_color=111111&fg_color=e88d00&link_color=e88d00&border_color=e88d00",
-        itchLink: "https://t3rrifk.itch.io/turn-the-tv-off",
-        itchTitle: "Turn the TV Off by T3rrifk"
+        itchEmbed: "https://itch.io/embed/3444867?border_width=2&bg_color=111111&fg_color=ed7100&link_color=ed7100&border_color=ed7100",
+        itchLink: "https://eeriepassion.itch.io/mayham-mansion",
+        itchTitle: "Mayham Mansion by T3rrifk"
     },
     {
         id: 2,
@@ -58,32 +58,6 @@ export const projects = [
         ],
         videoLink:"https://www.youtube.com/embed/bYQUJKPUhhI",
         videoTitle:"FingerBall Trailer",
-    },
-    {
-        id: 3,
-        title: "4 Dayz",
-        expertise: [
-            "SURIVIVAL HORROR GAME | ",
-            "PC | ",
-            "GAME JOLT | ",
-            "PUBLISHED"
-        ],
-        industry: "design industry",
-        solution: "4 Days is a survival horror game that I developed for GFGStudios and was published on Game Jolt as a commercial game. The game is set in a prison whereby the Player is locked inside with a blind serial killer. What makes the game unique is the enemy is completely blind and will not see the player but they hear every sound the player makes, even something as simple as walking and opening a drawer will attract the enemy. The goal of the game is for the player to find a way to escape while solving puzzles and completing objectives.",
-        link: "https://gamejolt.com/games/4dayz/773263",
-        link_2: "",
-        anchorText: "Download Game(Game Jolt)",
-        anchorText_2: "",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/30e50785430caf642c2ef64968fb6fdff6c457fd40dfe34ef16b8c6b2c90b44e.jpg",
-            "https://imgdb.net/storage/uploads/6ff0889de4ea3a488c6e8336455915820e3e70c006bc6266bb9f8e5dd6dc04bf.jpg",
-            "https://imgdb.net/storage/uploads/a7c4975b80045772d14e2860220f2169c4f20b4e5ec3916a2c927d9d882a5c63.jpg",
-            "https://imgdb.net/storage/uploads/566179c6267d4febb474e8706ce2438eedfdb7707f510d6bfbf489ae17db0364.jpg",
-            "https://imgdb.net/storage/uploads/59fdd461d03a26a06cd8dfb0b779cb347154c6adaa198fd5816d35a007c598a2.jpg"
-        ],
-        videoLink:"https://www.youtube.com/embed/G9vWfiwZ8T4",
-        videoTitle:"4 Dayz Release Trailer",
     },
     {
         id: 4,
@@ -137,27 +111,6 @@ export const projects = [
         videoTitle:"Confinement Walkthrough",
     },
     {
-        id: 6,
-        title: "Zomboid",
-        expertise: [
-            "HORROR GAME | ",
-            "RETRO | ",
-            "PC"
-        ],
-        industry: "design industry",
-        solution: "Zomboid is a retro game with PS1 graphics. I was hired as a Unity Developer to develop a prototype for a client who is a 3D Artist and animator. I was hired to develop the prototype from ground up and to add different mechanics such as the Player Controller, the gun mechanics, In-game map, Inventory management, complex Ai behavior and the ability for players to do counter attacks on enemies.",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/e7f51b1f2feef8cfe3b98f6eb336b8bdef12ac62bed20a5427cb1b91ef1b73d3.png",
-            "https://imgdb.net/storage/uploads/ed8be7f33fbac069495bf90512cc0db0ecb5576a42dda1d579b0f7e948e67bd2.png",
-            "https://imgdb.net/storage/uploads/fbb21d4938170dc3cc675e2553420b23aeaa06b98d55324612aa183bcca7d059.png",
-            "https://imgdb.net/storage/uploads/f5ac8c1020e41bb3f260ee60601d836c6f1741aaf42af7f5729f26e3313cde00.png",
-            "https://imgdb.net/storage/uploads/2b94c47a0d168960ad527960b5776cce5ecae0498b42e1180318541b2cf4cf24.png"
-        ],
-        videoLink:"https://www.youtube.com/embed/OKY9hDiIXTs",
-        videoTitle:"Zomboid",
-    },
-    {
         id: 7,
         title: "Run or Die",
         expertise: [
@@ -184,111 +137,6 @@ export const projects = [
     },
     {
         id: 8,
-        title: "They come from the lights",
-        expertise: [
-            "HORROR GAME | ",
-            "PUZZLE GAME | ",
-            "ADVENTURE GAME |",
-            "PC |"
-        ],
-        industry: "design industry",
-        solution: "They come from the lights is a first-person horror game that has the player wake up in their Apartment only to realize the entire city has been completely overrun by monsters. The only way for the Players to defend themselves is to use a UV flashlight which can chase away the monsters for a short while.",
-        link: "https://play.google.com/store/apps/details?id=com.SuperByteGames.Confinement",
-        link_2: "",
-        anchorText: "",
-        anchorText_2: "",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/35308252d3366bc6d5eca1fe65287cbcdf52acb494614fa818128838e34bfb62.jpg",
-            "https://imgdb.net/storage/uploads/1bab1e75aefe2fed035622b628b1ca4ce0bbd396f1cbcb777ce4e559f3ccba3f.jpg",
-            "https://imgdb.net/storage/uploads/cc5cce11bd9899c169b4973ae337d5df5d37e24f90ce0f6143216fb789d63fc2.jpg",
-            "https://imgdb.net/storage/uploads/0a9e31b1fe0c10ec0ecb645295b3f35af892e62acbe0043d4276494a451e1998.jpg",
-            "https://imgdb.net/storage/uploads/bab29ec00b13eda3fd519109998bc2d0356b7014bc798c034b6694da6c55f1e8.jpg"
-        ],
-        videoLink:"https://www.youtube.com/embed/crHGppJk5GQ",
-        videoTitle:"TiMMY",
-    }, {
-        id: 9,
-        title: "From the Darkness",
-        expertise: [
-            "HORROR GAME | ",
-            "PC"
-        ],
-        industry: "design industry",
-        solution: "The game is set in an abandoned train station and players take the role of an explorer trying to explore the train station tunnels. Whist exploring the mines, Players soon realize that there are some weird anomalies taking place. ",
-        link: "",
-        link_2: "",
-        anchorText: "",
-        anchorText_2: "",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/c24d1b96d907b18472e51c407e7cbfa66d5f5cb53fdf4e530445b05048ae27ed.png",
-            "https://imgdb.net/storage/uploads/c34e75c6278def37609aed6204272d0636ee7a63cc89075490100dd699e81e9c.png",
-            "https://imgdb.net/storage/uploads/04f18a649e6d35a1ca1c765cd7038f87d3e7c180bcb42a9eef720d43ea94b144.png",
-            "https://imgdb.net/storage/uploads/3fa8de4a4a33dc1ed2b9b8dcda53184a89c3b82e73b06572fafbd29b3051bdd5.png",
-            "https://imgdb.net/storage/uploads/f2af45405ef6165b0a2743c45a2859f209c6050493953c205d06ae9266565dde.png",
-            "https://imgdb.net/storage/uploads/83f82cb9a4e099812ab5c07e89e178213bc00252f8611e97dd5170721ce37532.png",
-            "https://imgdb.net/storage/uploads/9cd7ccda79412467f7840680ea44d049ebf115720a252ef4dc9fa9f8f130295c.png",
-            "https://imgdb.net/storage/uploads/de763f39a4f16f6e218d27474f52f2ea7f7d71d0953221c520aa34e742ab0b8c.png"
-        ],
-        videoLink:"https://www.youtube.com/embed/C-5xxRYwXEY",
-        videoTitle:"From the Darkness",
-    },
-    {
-        id: 10,
-        title: "TiMMY",
-        expertise: [
-            "HORROR GAME | ",
-            "PUZZLE GAME | ",
-            "PC"
-        ],
-        industry: "design industry",
-        solution: "TiMMY is a puzzle horror game whereby Players are locked inside a warehouse with a serial killer inside. To escape, Players have to find a key and solve 2 puzzles that the serial killer has setup up in the warehouse.",
-        link: "https://drive.google.com/file/d/114wWLDE2-lAVdQ3z1_D8iGoSiciOt5O_/view?usp=share_link",
-        link_2: "",
-        anchorText: "Download Game(Google Drive)",
-        anchorText_2: "",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/09820ecdf96195b87a1c0752be8bb254edcd9006b22267c8f0ca7f4a83ab5b92.png",
-            "https://imgdb.net/storage/uploads/ba7174bfa39156f2700789608982b5dde79d7e6424a23318f858a31cdb6ae3ce.png",
-            "https://imgdb.net/storage/uploads/b96b30dbdfde5f3405e6d4974b5d343004c85b9c5a6169ce9ed3130c0295f5ad.png",
-            "https://imgdb.net/storage/uploads/03a481c4f41dd014b97c63952e29b1cc640032f64aee0dde97c3232f31c32950.jpg"
-        ],
-        videoLink:"https://www.youtube.com/embed/nRKAwrnsDSk",
-        videoTitle:"TiMMY",
-    },
-    {
-        id: 11,
-        title: "Basement of Terror",
-        expertise: [
-            "HORROR GAME | ",
-            "PUZZLE GAME | ",
-            "MOBILE"
-        ],
-        industry: "design industry",
-        solution: " Basement of Terror is about a couple that locks a prisoner in their basement so they can pick the perfect candidate for a ritual. You play as the prisoner and you have to find a way to escape the Basement which has different rooms, corridors and puzzles. The objective of the game is to solve puzzles in the basement and each puzzle will lead to a special key of which the player will place on locked door. However, in this basement is also a Ghoulish like creature that has a very strong line of sight and will hear everything the player does from dropping objects to opening doors.",
-        link: "https://drive.google.com/file/d/14434wj6Q3E26w-k6ZMSZgdycJ1M39W4y/view?usp=share_link",
-        link_2: "",
-        anchorText: "Download Game(Google Drive)",
-        anchorText_2: "",
-        date: "10 November 2022",
-        images: [
-            "https://imgdb.net/storage/uploads/f3109b0d94d60dfe34bf379e5b6063c5aeb0ed5797fa1690b83f92e33071a9b5.jpg",
-            "https://imgdb.net/storage/uploads/4d2b83edf80960771266ff9cb648c15f209d4efdc46c3fdf37f296de04f022aa.jpg",
-            "https://imgdb.net/storage/uploads/b3469df4784447becc78c9c653cbc3b5707621203951c43b9b68be5b19b28bd0.jpg",
-            "https://imgdb.net/storage/uploads/6e8c2848933995dd2dcbbdd24dc889f0d107d8e131838bd9137362cc586cf37b.jpg",
-            "https://imgdb.net/storage/uploads/7a30c7c11199c8103374adac39430aa19a639ef8a1d357acd15e833122175355.jpg",
-            "https://imgdb.net/storage/uploads/694f981c5e283a4922738311000b97d2b9b0947017a5c4ee83f4066e3ba86e5e.jpg",
-            "https://imgdb.net/storage/uploads/a4a55a24e104ae0d979454efd41fe60ba3909f960c32051a67b7806799d899f7.jpg",
-            "https://imgdb.net/storage/uploads/6cda33bc77c0b62b441898ef8b132344748d0478e2ea4a6332fbf9dcbc9cb917.jpg"
-
-        ],
-        videoLink:"https://www.youtube.com/embed/GeyN_HeyadI",
-        videoTitle:"Basement of Terror",
-    },
-    {
-        id: 11,
         title: "Terror: Evocation II",
         expertise: [
             "SURIVIVAL HORROR GAME | ",
